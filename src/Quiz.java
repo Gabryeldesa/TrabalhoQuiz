20040a930d639feffa1ae4b2fec67caa15f726c5
Gabryeldesa <gabryelds8@gmail.com>
2024-11-26 19:55:01 -0300

    Merge remote-tracking branch 'origin/master'

--- src/Quiz.java
+++ src/Quiz.java
@@ -10,43 +10,43 @@ public class Quiz {
         ArrayList<Questao> questoes = new ArrayList<>();
         questoes.add(new Questao("1- Quantas notas musicais existem na escala cromática?",
                 "A) 7", "B) 8", "C) 12", "D) 10", "E) 5", "C"));
-        questoes.add(new Questao("Qual é a duração de uma semínima?",
+        questoes.add(new Questao("2- Qual é a duração de uma semínima?",
                 "A) 1 tempo", "B) 2 tempos", "C) Meio tempo", "D) 4 tempos", "E) 3 tempos", "A"));
-        questoes.add(new Questao("Qual é o intervalo de uma oitava?",
+        questoes.add(new Questao("3- Qual é o intervalo de uma oitava?",
                 "A) 5 tons", "B) 6 tons", "C) 7 tons", "D) 8 tons", "E) Meio tom", "C"));
-        questoes.add(new Questao("O que é um acorde maior?",
+        questoes.add(new Questao("4- O que é um acorde maior?",
                 "A) Tríade formada por uma terça maior e uma quinta justa", "B) Uma única nota",
                 "C) Tríade formada por terça menor e quinta diminuta", "D) Uma série de escalas",
                 "E) Uma tríade aumentada", "A"));
-        questoes.add(new Questao("Quantos semitons há em uma oitava?",
+        questoes.add(new Questao("5- Quantos semitons há em uma oitava?",
                 "A) 6", "B) 10", "C) 8", "D) 12", "E) 7", "D"));
-        questoes.add(new Questao("Qual é a sequência de tons da escala maior?",
+        questoes.add(new Questao("6- Qual é a sequência de tons da escala maior?",
                 "A) Tom, tom, semitom, tom, tom, tom, semitom", "B) Tom, semitom, tom, tom, semitom, tom, tom",
                 "C) Semitom, tom, tom, semitom, tom, tom, semitom", "D) Tom, tom, tom, tom, semitom",
                 "E) Semitom, tom, tom, tom, semitom, tom", "A"));
-        questoes.add(new Questao("O que significa 'pianíssimo' em música?",
+        questoes.add(new Questao("7- O que significa 'pianíssimo' em música?",
                 "A) Tocar rapidamente", "B) Tocar muito baixo", "C) Tocar com intensidade média",
                 "D) Tocar com força", "E) Tocar pausadamente", "B"));
-        questoes.add(new Questao("Qual é a fórmula para acordes diminutos?",
+        questoes.add(new Questao("8- Qual é a fórmula para acordes diminutos?",
                 "A) 1ª, 3ª maior e 5ª justa", "B) 1ª, 3ª menor e 5ª diminuta",
                 "C) 1ª, 3ª maior e 7ª maior", "D) 1ª, 5ª justa e 8ª", "E) 1ª, 3ª menor e 7ª menor", "B"));
-        questoes.add(new Questao("Qual é o tempo padrão de um compasso 4/4?",
+        questoes.add(new Questao("9- Qual é o tempo padrão de um compasso 4/4?",
                 "A) 3 tempos", "B) 6 tempos", "C) 4 tempos", "D) 2 tempos", "E) 8 tempos", "C"));
-        questoes.add(new Questao("O que é um intervalo harmônico?",
+        questoes.add(new Questao("10- O que é um intervalo harmônico?",
                 "A) Notas tocadas sucessivamente", "B) Notas tocadas simultaneamente",
                 "C) Uma escala menor", "D) Um ritmo alternado", "E) Uma melodia dissonante", "B"));
-        questoes.add(new Questao("Qual é a definição de 'timbre'?",
+        questoes.add(new Questao("11- Qual é a definição de 'timbre'?",
                 "A) Altura do som", "B) Duração do som", "C) Cor ou qualidade do som",
                 "D) Intensidade do som", "E) Frequência do som", "C"));
-        questoes.add(new Questao("O que é uma síncope na música?",
+        questoes.add(new Questao("12- O que é uma síncope na música?",
                 "A) Nota sustentada por vários compassos", "B) Ênfase em tempos fracos ou partes fracas do tempo",
                 "C) Sequência de notas descendentes", "D) Uma pausa longa", "E) Escala cromática", "B"));
-        questoes.add(new Questao("Quantos semitons há em um intervalo de quinta justa?",
+        questoes.add(new Questao("13- Quantos semitons há em um intervalo de quinta justa?",
                 "A) 6", "B) 5", "C) 7", "D) 8", "E) 4", "C"));
-        questoes.add(new Questao("O que significa 'adagio' na música?",
+        questoes.add(new Questao("14- O que significa 'adagio' na música?",
                 "A) Tocar rapidamente", "B) Tocar moderadamente rápido", "C) Tocar lentamente",
                 "D) Tocar com intensidade", "E) Tocar sem pausas", "C"));
-        questoes.add(new Questao("Qual é a nota intermediária entre Fá e Sol na escala cromática?",
+        questoes.add(new Questao("15- Qual é a nota intermediária entre Fá e Sol na escala cromática?",
                 "A) Fá sustenido", "B) Sol bemol", "C) Ambas estão corretas", "D) Nenhuma das anteriores",
                 "E) Lá bemol", "C"));
 
@@ -56,7 +56,6 @@ public class Quiz {
         System.out.println("Disciplina: Algoitmo e Programação II");
         System.out.println("Professor Breno Pimenta");
         System.out.println("------------------------------------------------------------------------------");
-        System.out.println("Olá, seja bem vindo ao Quiz!!!");
         System.out.println("Bem-vindo ao Quiz de Teoria Musical!");
         System.out.println("Você responderá 15 perguntas de múltipla escolha.");
         System.out.println("Cada pergunta tem 5 alternativas. Digite a letra correspondente à sua resposta.\n");
